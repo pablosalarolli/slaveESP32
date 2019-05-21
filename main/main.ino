47d50c4a7ea3d3444649421ed64b7e76c8209731
Pablo Salarolli <pablo_salarolli@hotmail.com>
2019-05-21 12:24:30 -0300

    First commit

---
+++ main/main.ino
@@ -0,0 +1,16 @@
+
+
+
+void setup() {
+  // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
+  pinMode(2,OUTPUT);
+  Serial2.begin(115200, SERIAL_8N1, 16, 17);
+}
+
+void loop() { //Choose Serial1 or Serial2 as required
+  while (Serial2.available()) {
+    digitalWrite(2,1);
+    Serial2.print(char(Serial2.read()));
+  }
+  digitalWrite(2,0);
+}
